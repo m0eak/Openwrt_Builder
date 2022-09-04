09aa5b3210480c142ea64d63c25e246a1d1b7bf8
m0eak <zhongliangpsp@sina.com>
2022-09-04 15:05:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,11 +17,11 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 rm -rf feeds.conf.default
 touch feeds.conf.default
-echo 'src-git packages https://github.com/zheshifandian/packages.git;axt1800' >>feeds.conf.default
+echo 'src-git packages https://github.com/m0eak/axt-packages.git' >>feeds.conf.default
 echo 'src-git routing https://github.com/openwrt/routing.git;openwrt-19.07' >>feeds.conf.default
 echo 'src-git telephony https://github.com/openwrt/telephony.git;openwrt-21.02' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git smallpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git luci https://github.com/zheshifandian/luci.git;axt1800' >>feeds.conf.default
+echo 'src-git smallpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
