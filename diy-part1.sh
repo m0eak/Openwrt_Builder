3533f2f9fe3bfe6a70bf7e81998faaa60a2b3f54
m0eak <zhongliangpsp@sina.com>
2022-09-24 11:22:53 +0800

    add homeproxy

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,3 +24,4 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf
 echo 'src-git luci https://github.com/zheshifandian/luci.git;axt1800' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
