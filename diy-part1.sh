c69871ad3765a8c69e6b12aa4ab82715646e9bcd
m0eak <zhongliangpsp@sina.com>
2023-06-30 10:09:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,14 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-rm -rf feeds.conf.default
-touch feeds.conf.default
-echo 'src-git packages https://github.com/zheshifandian/packages.git;axt1800' >>feeds.conf.default
-echo 'src-git routing https://github.com/openwrt/routing.git;openwrt-19.07' >>feeds.conf.default
-echo 'src-git telephony https://github.com/openwrt/telephony.git;openwrt-21.02' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git luci https://github.com/zheshifandian/luci.git;axt1800' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# rm -rf feeds.conf.default
+# touch feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
