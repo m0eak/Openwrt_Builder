0b51aeea807ba7039a5e3f1a38c95381258a9939
m0eak <zhongliangpsp@sina.com>
2023-09-02 17:47:05 +0800

    change feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # rm -rf feeds.conf.default
 # touch feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
