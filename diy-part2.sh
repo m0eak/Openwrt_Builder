5017f36320591ba7dbfc6664db974860498105bc
m0eak <zhongliangpsp@sina.com>
2023-09-14 11:55:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # Modify Openwrt to AXT1800
-#sed -i 's/'OpenWrt'/'AXT1800'/g' package/base-files/files/bin/config_generate
+sed -i 's/'OpenWrt'/'GL-AXT1800'/g' package/base-files/files/bin/config_generate
