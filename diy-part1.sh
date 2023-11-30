df54605d3130d02285a844700d9c88adf0b9c83a
m0eak <zhongliangpsp@sina.com>
2023-11-30 10:51:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,5 +22,4 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git smoothwan https://github.com/SmoothWAN/SmoothWAN-feeds' >>feeds.conf.default
 # fix cpu_opp_table
 sed -i '43s/0x3/0xf/;50s/0x3/0xf/;57s/0x1/0xf/' target/linux/qualcommax/patches-6.1/0154-arm64-dts-qcom-ipq6018-use-CPUFreq-NVMEM.patch
-git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
 
