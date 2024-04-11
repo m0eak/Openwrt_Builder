6bc04a99bef3d987d1d51546e26765c9e447a71e
m0eak <zhongliangpsp@sina.com>
2024-04-11 09:51:44 +0800

    Update axt-part2.sh

--- axt-part2.sh
+++ axt-part2.sh
@@ -14,3 +14,8 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # Modify Openwrt to AXT1800
 sed -i 's/'OpenWrt'/'GL-AXT1800'/g' package/base-files/files/bin/config_generate
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
