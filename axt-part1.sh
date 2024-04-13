015307b7a6d1b8ccb06419fa21b264c71605c7ef
m0eak <zhongliangpsp@sina.com>
2024-04-13 17:51:25 +0800

    Update axt-part1.sh

--- axt-part1.sh
+++ axt-part1.sh
@@ -18,6 +18,7 @@
 # rm -rf feeds.conf.default
 # touch feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> "feeds.conf"
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git smoothwan https://github.com/SmoothWAN/SmoothWAN-feeds' >>feeds.conf.default
