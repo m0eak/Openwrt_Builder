be100b3cebc802ada66cba34b8868ddf1bdc813f
m0eak <zhongliangpsp@sina.com>
2024-04-15 19:29:06 +0800

    Update mt3000-part1.sh

--- sh/mt3000-part1.sh
+++ sh/mt3000-part1.sh
@@ -31,4 +31,6 @@ echo "317eb6a6d9828371f8f0ca9cfaff251a" >> vermagic
 # sed -i '121s|grep '\''=[ym]'\'' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic|cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic|' ./include/kernel-defaults.mk
 # sed -i '121s#grep '\''=[ym]'\'' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic#cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic#' ./include/kernel-defaults.mk
 # curl -s https://downloads.immortalwrt.org/releases/23.05.2/targets/mediatek/filogic/immortalwrt-23.05.2-mediatek-filogic.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+sed -i '121s|^|# |' ./include/kernel-defaults.mk
+sed -i '121a\        cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic\' ./include/kernel-defaults.mk
 
