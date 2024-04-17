900b29b0b3317d16ea780aaf34ad40d125d505a0
m0eak <zhongliangpsp@sina.com>
2024-04-17 10:27:10 +0800

    Update mt3000-part2.sh

--- sh/mt3000-part2.sh
+++ sh/mt3000-part2.sh
@@ -11,9 +11,9 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' ./package/base-files/files/bin/config_generate
 # Modify ImmortalWrt to MT3000
-sed -i 's/'ImmortalWrt'/'GL-MT3000'/g' package/base-files/files/bin/config_generate
+sed -i 's/'ImmortalWrt'/'GL-MT3000'/g' ./package/base-files/files/bin/config_generate
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
