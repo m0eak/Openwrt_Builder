1ecd310282e689305bd1fc48153602fdeb8cbae8
m0eak <zhongliangpsp@sina.com>
2024-04-18 11:50:55 +0800

    Update mt3000-part2.sh

--- sh/mt3000-part2.sh
+++ sh/mt3000-part2.sh
@@ -16,7 +16,8 @@ sed -i 's/192.168.1.1/192.168.8.1/g' ./package/base-files/files/bin/config_gener
 sed -i 's/'ImmortalWrt'/'GL-MT3000'/g' ./package/base-files/files/bin/config_generate
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
-
+mkdir package/mtk
+git clone https://github.com/m0eak/luci-app-turboacc-mtk.git package/mtk
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 #./scripts/feeds install -a
