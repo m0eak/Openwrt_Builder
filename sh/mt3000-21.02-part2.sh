76ddcafc809f42bb62ee85026f671b6c9999c892
m0eak <zhongliangpsp@sina.com>
2024-04-19 11:47:48 +0800

    add 21.02

---
+++ sh/mt3000-21.02-part2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.8.1/g' ./package/base-files/files/bin/config_generate
+# Modify ImmortalWrt to MT3000
+sed -i 's/'ImmortalWrt'/'GL-MT3000'/g' ./package/base-files/files/bin/config_generate
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+# mkdir package/mtk
+# git clone https://github.com/m0eak/luci-app-turboacc-mtk.git package/mtk/luci-app-turboacc-mtk
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+./scripts/feeds install -a
