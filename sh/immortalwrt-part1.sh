5a1c59927d28c4ce532a4e9e35cafe40aff231c9
m0eak <zhongliangpsp@sina.com>
2024-05-05 14:28:44 +0800

    Update immortalwrt-part1.sh

--- sh/immortalwrt-part1.sh
+++ sh/immortalwrt-part1.sh
@@ -19,9 +19,10 @@
 # touch feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git linkease_nas https://github.com/linkease/nas-packages-luci' >>feeds.conf.default
-echo 'src-git linkease_nas_package https://github.com/linkease/nas-packages.git' >>feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore.git' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+# echo 'src-git linkease_nas https://github.com/linkease/nas-packages-luci' >>feeds.conf.default
+# echo 'src-git linkease_nas_package https://github.com/linkease/nas-packages.git' >>feeds.conf.default
+# echo 'src-git istore https://github.com/linkease/istore.git' >>feeds.conf.default
 # echo 'src-git kenzok https://github.com/kenzok8/small-package.git' >>feeds.conf.default
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 # 固定内核版本值
