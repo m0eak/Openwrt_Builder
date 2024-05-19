de99093df94f8e8203f98c56e07943a3ef88635a
m0eak <zhongliangpsp@sina.com>
2024-05-19 14:55:10 +0800

    Update immortalwrt-part2.sh

--- sh/immortalwrt-part2.sh
+++ sh/immortalwrt-part2.sh
@@ -17,8 +17,9 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # sed -i 's/'OpenWrt'/'GL-AXT1800'/g' package/base-files/files/bin/config_generate
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
+find ./ | grep Makefile | grep openclash | xargs rm -f
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
+git clone https://github.com/vernesong/OpenClash.git package/openclash
 ./scripts/feeds install -a
