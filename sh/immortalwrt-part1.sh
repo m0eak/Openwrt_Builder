87816106223c3e429edb803dda5c9c0cac44b2df
m0eak <zhongliangpsp@sina.com>
2024-05-26 16:12:30 +0800

    更改为curl

--- sh/immortalwrt-part1.sh
+++ sh/immortalwrt-part1.sh
@@ -26,9 +26,9 @@ echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.defau
 # echo 'src-git kenzok https://github.com/kenzok8/small-package.git' >>feeds.conf.default
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 # 固定内核版本值
-# curl -s https://downloads.immortalwrt.org/releases/23.05.2/targets/x86/64/immortalwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
-wget https://downloads.immortalwrt.org/releases/23.05.2/targets/x86/64/immortalwrt-23.05.2-x86-64.manifest
-grep kernel immortalwrt*.manifest | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+curl -s https://downloads.immortalwrt.org/releases/23.05.2/targets/x86/64/immortalwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+# wget https://downloads.immortalwrt.org/releases/23.05.2/targets/x86/64/immortalwrt-23.05.2-x86-64.manifest
+# grep kernel immortalwrt*.manifest | awk '{print $3}' | awk -F- '{print $3}' > vermagic
 # echo "317eb6a6d9828371f8f0ca9cfaff251a" > vermagic
 sed -i '121s|^|# |' ./include/kernel-defaults.mk
 sed -i $'121a\\\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic\\' ./include/kernel-defaults.mk
