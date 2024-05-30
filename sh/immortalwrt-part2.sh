f9062d678f5c7b0bf8fca4b8f87d00eaf43f9b91
m0eak <zhongliangpsp@sina.com>
2024-05-30 11:45:26 +0800

    update tailscale

--- sh/immortalwrt-part2.sh
+++ sh/immortalwrt-part2.sh
@@ -18,8 +18,17 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 find ./ | grep Makefile | grep openclash | xargs rm -f
+find ./ | grep Makefile | grep tailscale | xargs rm -f
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash
+# Update Tailscale
+git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+cd ..
+git clone --depth 1 https://github.com/immortalwrt/packages.git
+cp ./packages/net/tailscale ./openwrt/package/tailscale
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' openwrt/package/tailscale/Makefile
+rm -rf packages
+cd openwrt
 ./scripts/feeds install -a
