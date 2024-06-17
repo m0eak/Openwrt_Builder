eb39153e6bdab1a947efb5597695729132272ce0
m0eak <zhongliangpsp@sina.com>
2024-06-17 10:14:01 +0800

    Update immortalwrt-part2.sh

--- sh/immortalwrt-part2.sh
+++ sh/immortalwrt-part2.sh
@@ -18,11 +18,13 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 find ./ | grep Makefile | grep openclash | xargs rm -f
+find ./ | grep Makefile | grep ddns-go | xargs rm -f
 # find ./ | grep Makefile | grep tailscale | xargs rm -f
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash
+git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 # Update Tailscale
 git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 cd ..
