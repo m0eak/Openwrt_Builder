d7184fb22f8363484dc4c6605c4691e572f9c9d3
m0eak <zhongliangpsp@sina.com>
2024-06-17 10:14:19 +0800

    Update immortalwrt-part1.sh

--- sh/immortalwrt-part1.sh
+++ sh/immortalwrt-part1.sh
@@ -18,7 +18,6 @@
 # rm -rf feeds.conf.default
 # touch feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 # echo 'src-git linkease_nas https://github.com/linkease/nas-packages-luci' >>feeds.conf.default
