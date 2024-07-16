6091c3f5b0d0de9fe08cc039fa56a480248f1e3c
m0eak <zhongliangpsp@sina.com>
2024-07-16 12:38:00 +0800

    1

--- sh/scripts-part1.sh
+++ sh/scripts-part1.sh
@@ -20,15 +20,15 @@
 if [ "$(grep -c "AXT1800" $GITHUB_ENV)" -eq '1' ];then
   rm -rf feeds.conf.default
   touch feeds.conf.default
-  echo 'src-git packages https://git.openwrt.org/feed/packages.git' >>feeds.conf.default
-  echo 'src-git luci https://git.openwrt.org/project/luci.git' >>feeds.conf.default
-  echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
-  echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-  echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git;NSS-12.5-K6.x' >>feeds.conf.default
-  echo 'src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' >>feeds.conf.default
-  echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >>feeds.conf.default
-  echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-  echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+  echo 'src-git packages https://git.openwrt.org/feed/packages.git' >> feeds.conf.default
+  echo 'src-git luci https://git.openwrt.org/project/luci.git' >> feeds.conf.default
+  echo 'src-git routing https://git.openwrt.org/feed/routing.git' >> feeds.conf.default
+  echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
+  echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git;NSS-12.5-K6.x' >> feeds.conf.default
+  echo 'src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git' >> feeds.conf.default
+  echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
+  echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+  echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
   git checkout 0bd5323b7ad9e523584a156a0bd83881c4dea910 package/base-files/files/bin/config_generate && git checkout 0bd5323b7ad9e523584a156a0bd83881c4dea910 package/base-files/files/etc/banner && git checkout 0bd5323b7ad9e523584a156a0bd83881c4dea910 include/version.mk && git checkout 0bd5323b7ad9e523584a156a0bd83881c4dea910 package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh && echo "Done"
   echo "26768f9df0f6231779971745d5152147" > vermagic && echo "vermagic done"
   sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
@@ -38,13 +38,13 @@ if [ "$(grep -c "AXT1800" $GITHUB_ENV)" -eq '1' ];then
   # sed -i 's/16384/65536/g' ./package/kernel/linux/files/sysctl-nf-conntrack.conf
 fi
 if [ "$(grep -c "immortalwrt" $GITHUB_ENV)" -eq '1' ];then
-  echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+  echo 'src-git small https://github.com/kenzok8/small-package' >> feeds.conf.default
   curl -s https://downloads.immortalwrt.org/releases/23.05.2/targets/x86/64/immortalwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
   sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
   sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
 fi
 if [ "$(grep -c "MT3000" $GITHUB_ENV)" -eq '1' ];then
-  echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >>feeds.conf.default
+  echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
   # 修补的firewall4、libnftnl、nftables与952补丁
   curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
   # 调整cooling-levels
