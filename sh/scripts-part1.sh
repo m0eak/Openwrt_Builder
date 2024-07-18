78b49bb77e22e9869c41a93639eea308977f4e23
m0eak <zhongliangpsp@sina.com>
2024-07-18 13:24:37 +0800

    Update scripts-part1.sh

--- sh/scripts-part1.sh
+++ sh/scripts-part1.sh
@@ -42,20 +42,21 @@ if [ "$(grep -c "AXT-1800" $GITHUB_OUTPUT)" -eq '1' ] ;then
   # sed -i 's/16384/65536/g' ./package/kernel/linux/files/sysctl-nf-conntrack.conf
 fi
 if [ "$(grep -c "x86" $GITHUB_OUTPUT)" -eq '1' ];then
-  echo 'src-git small https://github.com/kenzok8/small-package' >> feeds.conf.default
   if [ "$(grep -c "immortalwrt" $GITHUB_OUTPUT)" -eq '1' ];then
-    curl -s https://downloads.immortalwrt.org/releases/$VERSION/targets/x86/64/immortalwrt-$VERSION-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+    curl -s https://downloads.immortalwrt.org/releases/$VERSION/targets/x86/64/immortalwrt-$VERSION-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic 
     sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
     sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
+    echo "Immortalwrt Vermagic Done"
   fi
   if [ "$(grep -c "Openwrt" $GITHUB_OUTPUT)" -eq '1' ];then
     # 修补的firewall4、libnftnl、nftables与952补丁
     # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-    echo 'src-git smpackage https://github.com/kenzok8/small-package' >> feeds.conf.default
     curl -s https://downloads.openwrt.org/releases/$VERSION/targets/x86/64/openwrt-$VERSION-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
     sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
     sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
+    echo "Openwrt Vermagic Done"
   fi
+  echo 'src-git small https://github.com/kenzok8/small-package' >> feeds.conf.default
 fi
 if [ "$(grep -c "MT-3000" $GITHUB_OUTPUT)" -eq '1' ];then
   echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
