50a05b63f3b1406086ddb52d67ebfb1158388f35
m0eak <zhongliangpsp@sina.com>
2024-11-21 11:27:10 +0800

    Update scripts-part2.sh

--- sh/scripts-part2.sh
+++ sh/scripts-part2.sh
@@ -11,29 +11,29 @@
 #
 
 # Modify default IP
-#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-#find ./ | grep Makefile | grep mosdns | xargs rm -f
-#find ./ | grep Makefile | grep openclash | xargs rm -f
-#find ./ | grep Makefile | grep ddns-go | xargs rm -f
-#find ./ | grep Makefile | grep homeproxy | xargs rm -f
-#find ./ | grep Makefile | grep wolplus | xargs rm -f
-#find ./ | grep Makefile | grep luci-theme-design | xargs rm -f
-#find ./ | grep Makefile | grep design-config | xargs rm -f
-#if [ "$(grep -c "AXT-1800" $GITHUB_OUTPUT)" -eq '1' ];then
-#  sed -i 's/192.168.1.1/192.168.8.1/g' ./package/base-files/files/bin/config_generate
-#  sed -i 's/'OpenWrt'/'GL-AXT1800'/g' ./package/base-files/files/bin/config_generate
-#  find ./ | grep Makefile | grep app-store | xargs rm -f
-#  find ./ | grep Makefile | grep linkease | xargs rm -f
-#  find ./ | grep Makefile | grep linkmount | xargs rm -f
-#  find ./ | grep Makefile | grep quickstart | xargs rm -f
-#  find ./ | grep Makefile | grep turboacc | xargs rm -f
-#  find ./ | grep Makefile | grep unishare | xargs rm -f
-#  find ./ | grep Makefile | grep webdav2 | xargs rm -f
-#  git clone --depth 1 https://github.com/linkease/istore.git package/istore
-#  git clone --depth 1 https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
-#  git clone --depth 1 https://github.com/linkease/nas-packages.git package/nas-packages
-#  git clone --depth 1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
-#  git clone --depth 1 https://github.com/chenmozhijin/turboacc.git package/turboacc
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+find ./ | grep Makefile | grep openclash | xargs rm -f
+find ./ | grep Makefile | grep ddns-go | xargs rm -f
+find ./ | grep Makefile | grep homeproxy | xargs rm -f
+find ./ | grep Makefile | grep wolplus | xargs rm -f
+find ./ | grep Makefile | grep luci-theme-design | xargs rm -f
+find ./ | grep Makefile | grep design-config | xargs rm -f
+if [ "$(grep -c "AXT-1800" $GITHUB_OUTPUT)" -eq '1' ];then
+  sed -i 's/192.168.1.1/192.168.8.1/g' ./package/base-files/files/bin/config_generate
+  sed -i 's/'OpenWrt'/'GL-AXT1800'/g' ./package/base-files/files/bin/config_generate
+  find ./ | grep Makefile | grep app-store | xargs rm -f
+  find ./ | grep Makefile | grep linkease | xargs rm -f
+  find ./ | grep Makefile | grep linkmount | xargs rm -f
+  find ./ | grep Makefile | grep quickstart | xargs rm -f
+  find ./ | grep Makefile | grep turboacc | xargs rm -f
+  find ./ | grep Makefile | grep unishare | xargs rm -f
+  find ./ | grep Makefile | grep webdav2 | xargs rm -f
+  git clone --depth 1 https://github.com/linkease/istore.git package/istore
+  git clone --depth 1 https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
+  git clone --depth 1 https://github.com/linkease/nas-packages.git package/nas-packages
+  git clone --depth 1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+  git clone --depth 1 https://github.com/chenmozhijin/turboacc.git package/turboacc
 #  # sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 #  echo "axt1800 part-2"
 #fi
@@ -54,12 +54,12 @@ if [ "$(grep -c "MT-3000" $GITHUB_OUTPUT)" -eq '1' ];then
   sed -i 's/'ImmortalWrt'/'GL-MT3000'/g' ./package/base-files/files/bin/config_generate
   echo "MT3000 part-2"
 fi
-#git clone --depth 1 https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
-#git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-#git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-#git clone --depth 1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
-#git clone --depth 1 https://github.com/m0eak/openwrt_patch.git ./package/custom
+git clone --depth 1 https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash
+git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone --depth 1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone --depth 1 https://github.com/m0eak/openwrt_patch.git ./package/custom
 # git clone -b js --depth 1 https://github.com/papagaye744/luci-theme-design.git package/luci-theme-design
 # git clone --depth 1 https://github.com/xuanranran/luci-app-design-config.git package/luci-app-design-config
