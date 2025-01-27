4ef71a4a558e1994def0125c3d023cb78fef79f6
m0eak <zhongliangpsp@sina.com>
2025-01-27 11:28:41 +0800

    调整

--- sh/scripts-part2.sh
+++ sh/scripts-part2.sh
@@ -19,7 +19,7 @@ NC='\033[0m' # No Color
 TARGET_DIR="$PWD/package"
 echo "检查 TARGET_DIR: $TARGET_DIR"
 if [ ! -d "$TARGET_DIR" ]; then
-    echo -e "${RED} $home 目录下未找到 源码仓库，请确保源码仓库在目录下，请善用 mv 命令移动源码仓库 ${NC}"
+    echo -e "${RED} $PWD/package 目录下未找到 源码仓库，请确保源码仓库在目录下，请善用 mv 命令移动源码仓库 ${NC}"
     exit 1
 fi
 
@@ -35,6 +35,27 @@ REPOS=(
     ["https://github.com/0x676e67/luci-theme-design"]="js"  # 指定 js 分支
 )
 
+# 提取仓库名称列表
+declare -a repo_names
+for repo_url in "${!REPOS[@]}"; do
+    repo_names+=("$(basename -s .git "$repo_url")")
+done
+
+# 删除所有符合条件的 Makefile
+echo "开始查找并删除符合条件的 Makefile"
+find . -type f -name "Makefile" ! -path "$PWD/package*" |
+while IFS= read -r file; do
+  for repo_name in "${repo_names[@]}"; do
+    if [[ "$file" == *"$repo_name"* ]]; then
+      echo "删除 Makefile: $file"
+      rm -f "$file"
+      break # 找到匹配的仓库名称，跳出内层循环
+    fi
+  done
+done
+echo "Makefile 清理完成"
+
+
 update_or_clone_repo() {
     local repo_url=$1
     local repo_branch=${REPOS[$repo_url]}
@@ -43,24 +64,34 @@ update_or_clone_repo() {
 
     echo "处理仓库: $repo_name, URL: $repo_url, 分支: $repo_branch"
 
-    # 排除 $PWD/package 目录及其子目录下的 Makefile
-    find . -type f -name "Makefile" ! -path "$PWD/package*" | grep "$repo_name" | xargs rm -f
-    echo "Makefile 清理完成"
-
     if [ ! -d "$repo_dir" ]; then
         echo -e "${GREEN}Cloning $repo_name${NC}"
         if [ -z "$repo_branch" ]; then
             echo "执行 git clone (默认分支): git clone --single-branch --depth 1 \"$repo_url\" \"$repo_dir\""
             git clone --single-branch --depth 1 "$repo_url" "$repo_dir"
+            echo "仓库 $repo_name 克隆完成"
         else
             echo "执行 git clone (指定分支): git clone --single-branch --depth 1 -b \"$repo_branch\" \"$repo_url\" \"$repo_dir\""
             git clone --single-branch --depth 1 -b "$repo_branch" "$repo_url" "$repo_dir"
+            echo "仓库 $repo_name 克隆完成"
         fi
     else
         echo -e "${GREEN}Updating $repo_name${NC}"
         cd "$repo_dir" || exit
+        echo "执行 git restore ."
+        git restore .
+        if [ $? -eq 0 ]; then
+          echo "仓库 $repo_name Makefile 恢复完成"
+        else
+          echo "仓库 $repo_name Makefile 恢复失败"
+        fi
         echo "执行 git pull"
         git pull
+        if [ $? -eq 0 ]; then
+          echo "仓库 $repo_name 更新完成"
+        else
+          echo "仓库 $repo_name 更新失败"
+        fi
         cd - || exit
     fi
     echo "仓库 $repo_name 处理完成"
