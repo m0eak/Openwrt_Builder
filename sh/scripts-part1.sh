10b7c709d065bb3d5ab23f4cdb751b26fc2c7b77
m0eak <zhongliangpsp@sina.com>
2025-03-16 22:51:02 +0900

    Update scripts-part1.sh

--- sh/scripts-part1.sh
+++ sh/scripts-part1.sh
@@ -51,14 +51,15 @@ if [ "$(grep -c "x86" $GITHUB_OUTPUT)" -eq '1' ];then
   fi
 fi
 if [ "$(grep -c "MT-3000" $GITHUB_OUTPUT)" -eq '1' ];then
-  echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
+  # echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
   # 修补的firewall4、libnftnl、nftables与952补丁
-  curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
   # 调整cooling-levels
   wget https://raw.githubusercontent.com/m0eak/openwrt_patch/main/mt3000/980-dts-mt7921-add-cooling-levels.patch 
   mv 980-dts-mt7921-add-cooling-levels.patch ./target/linux/mediatek/patches-5.15/980-dts-mt7921-add-cooling-levels.patch 
+  rm -rf feeds/packages/lang/golang
+  git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
   # 固定内核版本值
-  curl -s https://downloads.immortalwrt.org/releases/$VERSION/targets/mediatek/filogic/immortalwrt-$VERSION-mediatek-filogic.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic && echo "MT3000 Vermagic Done" && echo "当前Vermagic：" && cat vermagic
-  sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
-  sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
+  # curl -s https://downloads.immortalwrt.org/releases/$VERSION/targets/mediatek/filogic/immortalwrt-$VERSION-mediatek-filogic.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic && echo "MT3000 Vermagic Done" && echo "当前Vermagic：" && cat vermagic
+  # sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
+  # sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
 fi
