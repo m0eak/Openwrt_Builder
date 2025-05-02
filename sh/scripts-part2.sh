1623da5e78c08466793e14130c9b88a786727e32
m0eak <zhongliangpsp@sina.com>
2025-05-02 13:28:24 +0800

    add design config

--- sh/scripts-part2.sh
+++ sh/scripts-part2.sh
@@ -23,6 +23,7 @@ declare -A REPOS=(
     ["https://github.com/animegasan/luci-app-wolplus"]="" # 使用默认分支
     ["https://github.com/m0eak/luci-theme-asus"]="js"  # 指定 js 分支
     ["https://github.com/0x676e67/luci-theme-design"]="js"  # 指定 js 分支
+    ["https://github.com/0x676e67/luci-app-design-config.git"]="" # 使用默认分支
     ["https://github.com/nikkinikki-org/OpenWrt-nikki"]="" # 使用默认分支
 )
 
