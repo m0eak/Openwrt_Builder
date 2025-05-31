28675226a1c45ed76880fdf15b264e0c197b2028
m0eak <zhongliangpsp@sina.com>
2025-05-31 09:30:01 +0800

    Update scripts-part1.sh

--- sh/scripts-part1.sh
+++ sh/scripts-part1.sh
@@ -77,12 +77,12 @@ if [ "$(grep -c "x86" $GITHUB_OUTPUT)" -eq '1' ];then
     git clone https://github.com/chenmozhijin/turboacc.git -b package ./package/turboacc-package
     cd ./package/turboacc-package && ls && find . -maxdepth 1 -type d ! -name 'shortcut-fe' && find . -maxdepth 1 -type d ! -name 'shortcut-fe' -exec rm -r {} +
     cd ../..   
-  if [ ! -s ./vermagic ]; then
+    if [ ! -s ./vermagic ]; then
     echo "none vermagic"
-  else
+    else
     sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
     sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
-  fi
+    fi
   if [ "$(grep -c "Openwrt" $GITHUB_OUTPUT)" -eq '1' ];then
     # 修补的firewall4、libnftnl、nftables与952补丁
     # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
@@ -94,6 +94,7 @@ if [ "$(grep -c "x86" $GITHUB_OUTPUT)" -eq '1' ];then
   fi
 fi
 if [ "$(grep -c "TR-3000" $GITHUB_OUTPUT)" -eq '1' ];then
+  echo "TR-3000"
   # 调整cooling-levels
   # wget https://raw.githubusercontent.com/m0eak/openwrt_patch/main/mt3000/980-dts-mt7921-add-cooling-levels.patch 
   # mv 980-dts-mt7921-add-cooling-levels.patch ./target/linux/mediatek/patches-5.15/980-dts-mt7921-add-cooling-levels.patch 
