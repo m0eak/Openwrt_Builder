8585555f6fe9665e3f1765cf0282f990e9df0bbc
m0eak <zhongliangpsp@sina.com>
2025-08-16 18:06:35 +0800

    Update scripts-part2.sh

--- sh/scripts-part2.sh
+++ sh/scripts-part2.sh
@@ -116,5 +116,6 @@ else
     echo "找到 Makefile，继续执行"
 fi
 
-rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang && echo "删除golang"
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+cat feeds/packages/lang/golang/golang/Makefile
