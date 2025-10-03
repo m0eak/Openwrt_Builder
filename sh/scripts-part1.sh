2996e64abb7c9176ae983f1c215b6d41608d260f
m0eak <zhongliangpsp@sina.com>
2025-10-03 21:45:49 +0800

    add ipaddr

--- sh/scripts-part1.sh
+++ sh/scripts-part1.sh
@@ -50,7 +50,9 @@ if [ "$(grep -c "AXT-1800" $GITHUB_OUTPUT)" -eq '1' ] ;then
   # 输出结果
   echo "提取的内核版本号: $KERNEL_VERSION"
   echo "你可以通过 \$KERNEL_VERSION 变量使用这个值"
-  # sed -i '63s/0x1/0xf/;70s/0x1/0xf/' ./target/linux/qualcommax/patches-6.12/0054-v6.8-arm64-dts-qcom-ipq6018-use-CPUFreq-NVMEM.patch && echo "CPUFreq Done"
+
+  sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+  
   rm -rf feeds/packages/lang/golang && echo "删除golang"
   git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
   cat feeds/packages/lang/golang/golang/Makefile
@@ -70,12 +72,13 @@ fi
 if [ "$(grep -c "x86" $GITHUB_OUTPUT)" -eq '1' ];then
   if [ "$(grep -c "immortalwrt" $GITHUB_OUTPUT)" -eq '1' ];then
     rm -rf feeds/packages/lang/golang && echo "删除golang"
-    git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+    git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
     cat feeds/packages/lang/golang/golang/Makefile
-    #rm -rf feeds/packages/lang/golang && echo "删除golang"
-    #git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-    #cat feeds/packages/lang/golang/golang/Makefile
+    
+    sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+    
     sed -i "s/replace/$VERSION2/g" $GITHUB_WORKSPACE/files/etc/uci-defaults/zzz_m0eak && echo "VERSION替换成功"
+    
     curl -s https://downloads.immortalwrt.org/releases/$VERSION2/targets/x86/64/immortalwrt-$VERSION2-x86-64.manifest | grep kernel | awk '{print $3}' | sed -E 's/.*~([0-9a-f]+)-r[0-9]+$/\1/; s/.*-([0-9a-f]+)$/\1/' > vermagic && echo "Immortalwrt Vermagic Done" && echo "当前Vermagic：" && cat vermagic
     sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
     #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
@@ -91,25 +94,8 @@ if [ "$(grep -c "x86" $GITHUB_OUTPUT)" -eq '1' ];then
     sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
     fi
   fi
-  if [ "$(grep -c "Openwrt" $GITHUB_OUTPUT)" -eq '1' ];then
-    # 修补的firewall4、libnftnl、nftables与952补丁
-    # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-    sed -i "s/replace/$VERSION/g" $GITHUB_WORKSPACE/files/etc/uci-defaults/zzz_m0eak && echo "VERSION替换成功"
-    curl -s https://downloads.openwrt.org/releases/$VERSION/targets/x86/64/openwrt-$VERSION-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic && echo "Openwrt Vermagic Done" && echo "当前Vermagic：" && cat vermagic
-    sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
-    sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
-    echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >> feeds.conf.default
-  fi
 fi
 if [ "$(grep -c "TR-3000" $GITHUB_OUTPUT)" -eq '1' ];then
   echo "TR-3000"
-  # 调整cooling-levels
-  # wget https://raw.githubusercontent.com/m0eak/openwrt_patch/main/mt3000/980-dts-mt7921-add-cooling-levels.patch 
-  # mv 980-dts-mt7921-add-cooling-levels.patch ./target/linux/mediatek/patches-5.15/980-dts-mt7921-add-cooling-levels.patch 
-  # rm -rf feeds/packages/lang/golang
-  # git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-  # 固定内核版本值
-  # curl -s https://downloads.immortalwrt.org/releases/$VERSION/targets/mediatek/filogic/immortalwrt-$VERSION-mediatek-filogic.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic && echo "MT3000 Vermagic Done" && echo "当前Vermagic：" && cat vermagic
-  # sed -i '/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/\.config\.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/\.vermagic/s/^/# /' ./include/kernel-defaults.mk
-  # sed -i '/$(LINUX_DIR)\/\.vermagic/a \\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' ./include/kernel-defaults.mk
+  sed -i 's/192.168.1.1/192.168.100.209/g' package/base-files/files/bin/config_generate
 fi
